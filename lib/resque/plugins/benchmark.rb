e90c37f5988b10df46c4eb3cd227ec93b7e7561f
Adam <electronicbattleweapon7@gmail.com>
2012-06-27 18:11:24 +0100

    logging

--- lib/resque/plugins/benchmark.rb
+++ lib/resque/plugins/benchmark.rb
@@ -1,6 +1,9 @@
 module Resque
   module Plugins
     module Benchmark
+      def after_enqueue_with_benchmarking(*args)
+        RESQUE_LOGGER.info "Job: #{self}, Args: #{args.inspect}"
+      end
     end
   end
 end
