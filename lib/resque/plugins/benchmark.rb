e460bcf704c3156268dc687ac3cd7f4fe5d4f7fe
Adam <electronicbattleweapon7@gmail.com>
2012-06-28 14:43:17 +0100

    get start and finish times, store diff in a set
    TODO: do something usefull with the set.

--- lib/resque/plugins/benchmark.rb
+++ lib/resque/plugins/benchmark.rb
@@ -2,7 +2,26 @@ module Resque
   module Plugins
     module Benchmark
       def after_enqueue_with_benchmarking(*args)
-        RESQUE_LOGGER.info "Job: #{self}, Args: #{args.inspect}"
+        job = self.name
+        id = args[0]
+        start_time_key = "benchmark:started:#{job}:#{id}"
+        Resque.redis.set start_time_key, Time.now.to_i.to_s
+        start_time = Resque.redis.get start_time_key
+      end
+
+      def after_perform_with_benchmarking(*args)
+        job = self.name
+        id = args[0]
+
+        start_time_key = "benchmark:started:#{job}:#{id}"
+
+        start_time = Resque.redis.get start_time_key
+        finish_time = Time.now.to_i
+        duration = finish_time.to_i - start_time.to_i
+
+        key = "benchmark:aggregate:#{job}"
+        Resque.redis.sadd key, duration.to_s
+        Resque.redis.del start_time_key
       end
     end
   end
